f3f56dfa1fbb62e84e7693688a6239093d4760d2
mike123789-dev <mike123789@g.skku.edu>
2021-01-20 20:25:20 +0900

    feat: voca brain 모델 구현

---
+++ Voca/Voca/Models/VocaBrain.swift
@@ -0,0 +1,35 @@
+//
+//  VocaBrain.swift
+//  LearnCombine
+//
+//  Created by 강병민 on 2021/01/20.
+//
+
+import Foundation
+
+class VocaBrain {
+    let voca : Voca
+    let addedDate = Date()
+    var isFavorite: Bool = false
+    
+    private var correctCount = 0
+    private var wrongCount = 0
+    var totalCount: Int {
+        correctCount + wrongCount
+    }
+    var correctAnswerRate : Double{
+        Double(correctCount)/Double(totalCount)
+    }
+    
+    init(voca : Voca) {
+        self.voca = voca
+    }
+    
+    func answerWrong(){
+        wrongCount = wrongCount + 1
+    }
+    func answerCorrect(){
+        correctCount = correctCount + 1
+    }
+    
+}
