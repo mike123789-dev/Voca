de98aace3bbefe40eebe8772356ad9cde7fce358
mike123789-dev <mike123789@g.skku.edu>
2021-01-20 20:25:02 +0900

    feat: voca 모델 구현

---
+++ Voca/Voca/Models/Voca.swift
@@ -0,0 +1,14 @@
+//
+//  Voca.swift
+//  VocaApp
+//
+//  Created by 강병민 on 2020/10/19.
+//
+
+import Foundation
+
+struct Voca : Hashable {
+    let id = UUID()
+    let question : String
+    let answer : String
+}
