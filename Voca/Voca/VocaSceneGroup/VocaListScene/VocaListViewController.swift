1210efe2afc69e8475932057b5b1b473501aad22
mike123789-dev <mike123789@g.skku.edu>
2021-01-20 20:41:18 +0900

    feat: VocaList Scene을 위한 파일 생성
    - ViewController 추가
    - ViewModel 추가

---
+++ Voca/Voca/VocaSceneGroup/VocaListScene/VocaListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  VocaListViewController.swift
+//  Voca
+//
+//  Created by 강병민 on 2021/01/20.
+//
+
+import UIKit
+
+class VocaListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Voca/Voca/VocaSceneGroup/VocaListScene/VocaListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  VocaListViewModel.swift
+//  Voca
+//
+//  Created by 강병민 on 2021/01/20.
+//
+
+import Foundation
