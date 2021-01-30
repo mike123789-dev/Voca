6da62600568bf17022b70c339e5914ac947343d0
mike123789-dev <mike123789@g.skku.edu>
2021-01-31 00:53:21 +0900

    feat: VocaCameraViewController 추가

---
+++ Voca/Voca/VocaSceneGroup/VocaCameraScene/VocaCameraViewController.swift
@@ -0,0 +1,17 @@
+//
+//  VocaCameraViewController.swift
+//  Voca
+//
+//  Created by 강병민 on 2021/01/31.
+//
+
+import UIKit
+import AVFoundation
+
+class VocaCameraViewController: UIViewController {
+    @IBOutlet weak var capturePreview: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
