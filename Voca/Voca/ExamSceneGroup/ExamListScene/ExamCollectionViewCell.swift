b31e1769d0dd1da8f8c4b900760858d61cb11506
mike123789-dev <mike123789@g.skku.edu>
2021-02-01 00:40:26 +0900

    feat: ExamCollectionViewCell 구현

--- Voca/Voca/ExamSceneGroup/ExamListScene/ExamCollectionViewCell.swift
+++ Voca/Voca/ExamSceneGroup/ExamListScene/ExamCollectionViewCell.swift
@@ -8,5 +8,20 @@
 import UIKit
 
 class ExamCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var folderLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var vocaCountLabel: UILabel!
+    @IBOutlet weak var favoriteVocaCountLabel: UILabel!
+    @IBOutlet weak var averageScoreLabel: UILabel!
+    @IBOutlet weak var RecentScoreLabel: UILabel!
+    
+    func configure(with folder: VocaSectionModel) {
+        folderLabel.text = folder.title
+//        dateLabel.text = folder.date.description
+        vocaCountLabel.text = "총 \(folder.vocas.count) 단어"
+        favoriteVocaCountLabel.text = "😃"
+        averageScoreLabel.text = "70 %"
+        RecentScoreLabel.text = "80 %"
+    }
     
 }
