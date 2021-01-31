d034756d9c74da1abef6bf2db4c1282328c0c306
mike123789-dev <mike123789@g.skku.edu>
2021-02-01 00:40:10 +0900

    feat: ExamHeaderCollectionReusableView 구현

--- Voca/Voca/ExamSceneGroup/ExamListScene/ExamHeaderCollectionReusableView.swift
+++ Voca/Voca/ExamSceneGroup/ExamListScene/ExamHeaderCollectionReusableView.swift
@@ -8,5 +8,32 @@
 import UIKit
 
 class ExamHeaderCollectionReusableView: UICollectionReusableView {
-        
+    let label = UILabel()
+    static let reuseIdentifier = "title-supplementary-reuse-identifier"
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func setup() {
+        addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        let inset = CGFloat(8)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: inset),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -inset),
+            label.topAnchor.constraint(equalTo: topAnchor, constant: inset),
+            label.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -inset)
+        ])
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+    }
+    
+    func configure(title: String) {
+        label.text = title
+    }
 }
